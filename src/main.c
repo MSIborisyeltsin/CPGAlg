518d2d6508d740c31c5cb67e6b7c6c2a3e6eb790
Шеогорат <95223648+MSIborisyeltsin@users.noreply.github.com>
2024-04-12 00:22:24 +0700

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "hashmap.h"
+
+int main()
+{
+    HashMap* encryptionMap = createHashMap();
+    put(encryptionMap, "A", 117);
+    put(encryptionMap, "B", 205);
+
+    // Example usage
+    char* key = "A";
+    int value = get(encryptionMap, key);
+    printf("Value for key %s: %d\n", key, value);
+
+    destroyHashMap(encryptionMap);
+    return 0;
+}
