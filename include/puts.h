ba1dc859d34796567fa4bdc233f41df9fc0e7540
Шеогорат <95223648+MSIborisyeltsin@users.noreply.github.com>
2024-04-13 22:35:40 +0700

    Update puts.h

--- include/puts.h
+++ include/puts.h
@@ -4,8 +4,58 @@
 
 void addValues(HashMap* map)
 {
-    put(map, "A", 117);
-    put(map, "B", 205);
+    put(map, "A", 57767); // Aa
+    put(map, "a", 53643);
+    put(map, "B", 49741); // Bb
+    put(map, "b", 21327);
+    put(map, "C", 71088); // Cc
+    put(map, "c", 15358);
+    put(map, "D", 63796); // Dd
+    put(map, "d", 72015);
+    put(map, "E", 47139); // Ee
+    put(map, "e", 31868);
+    put(map, "F", 21455); // Ff
+    put(map, "f", 10095);
+    put(map, "G", 54063); // Gg
+    put(map, "g", 16096);
+    put(map, "H", 98257); // Hh
+    put(map, "h", 73011);
+    put(map, "I", 50928); // Ii
+    put(map, "i", 91478);
+    put(map, "J", 83904); // Jj
+    put(map, "j", 33500);
+    put(map, "K", 53115); // Kk
+    put(map, "k", 54985);
+    put(map, "L", 23596); // Ll
+    put(map, "l", 24453);
+    put(map, "M", 94676); // Mm
+    put(map, "m", 70985);
+    put(map, "N", 28476); // Nn
+    put(map, "n", 20088);
+    put(map, "O", 51801); // Oo
+    put(map, "o", 44214);
+    put(map, "P", 91807); // Pp
+    put(map, "p", 88295);
+    put(map, "Q", 88259); // Qq
+    put(map, "q", 14737);
+    put(map, "R", 17900); // Rr
+    put(map, "r", 69114);
+    put(map, "S", 97759); // Ss
+    put(map, "s", 53618);
+    put(map, "T", 81881); // Tt
+    put(map, "t", 67886);
+    put(map, "U", 31503); // Uu
+    put(map, "u", 17987);
+    put(map, "V", 29962); // Vv
+    put(map, "v", 11238);
+    put(map, "W", 21709); // Ww
+    put(map, "w", 10229);
+    put(map, "X", 25297); // Xx
+    put(map, "x", 33515);
+    put(map, "Y", 65943); // Yy
+    put(map, "y", 76477);
+    put(map, "Z", 94433); // Zz
+    put(map, "z", 31275);
 }
 
 #endif
