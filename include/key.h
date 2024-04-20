ea6ed99611e5b0a21902b87b13f08ef26e64a414
Шеогорат <95223648+MSIborisyeltsin@users.noreply.github.com>
2024-04-21 00:13:58 +0700

    Create key.h
    Checking the key size

---
+++ include/key.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef KEY_SIZE_CHECK_H
+#define KEY_SIZE_CHECK_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void checkKeySize(char str[])
+{
+    int bits = strlen(str) * 8;
+
+    if(bits != 64 && bits != 128 && bits != 256){
+        printf("Error: String size must be 64, 128, or 256 bits.\n");
+        exit(1);
+    }
+
+    printf("KeySize: %d\n", bits);
+}
+
+#endif
