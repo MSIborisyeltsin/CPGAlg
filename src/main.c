33124fce6116a6e46a81bb433ad2f5904c2391be
Шеогорат <95223648+MSIborisyeltsin@users.noreply.github.com>
2024-04-21 11:41:32 +0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "../include/hashmap.h"
 #include "../include/puts.h"
 
@@ -7,10 +10,22 @@ int main()
     HashMap* encryptionMap = createHashMap();
     addValues(encryptionMap);
 
-    // Example usage
-    char* key = "A";
-    int value = get(encryptionMap, key);
-    printf("Value for key %s: %d\n", key, value);
+    unsigned char input[255];
+    printf("Enter a string: ");
+    fgets(input, 255, stdin);
+
+    // Remove newline character from input
+    input[strcspn(input, "\n")] = 0;
+
+    // Iterate through the characters in the input string
+    for (int i = 0; i < strlen(input); i++) {
+        char key[2];
+        key[0] = input[i];
+        key[1] = '\0';
+
+        int value = get(encryptionMap, key);
+        printf("Value for key %c: %d\n", input[i], value);
+    }
 
     destroyHashMap(encryptionMap);
     return 0;
